e6c39879e83b8ad9df98c8443f5b4f7d5fff231c
Javier Jaramillo <jjaramillo34@gmail.com>
2023-12-16 21:34:36 -0500

    [chrome]: Update vercel analytics

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import AppRouter from "./Router";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <AppRouter />
       <SpeedInsights />
+      <Analytics />
     </div>
   );
 }
